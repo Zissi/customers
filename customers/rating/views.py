8283cf19e5f9530836b499c155106ddff57a525e
zissi <f.schlesinger@gmx.net>
2015-03-30 11:32:42 +0200

    view to display list of customers added and template for it created

--- customers/rating/views.py
+++ customers/rating/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from rating.models import CustomerRating
 
-# Create your views here.
+class CustomerView(generic.ListView):
+    template_name = 'rating/customers.html'
+    context_object_name = 'customers'
+    
+    def get_queryset(self):
+        return CustomerRating.objects.filter()
