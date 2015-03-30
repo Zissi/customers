bd5979195e14b1d91c7649e9244404c997ab7b62
zissi <f.schlesinger@gmx.net>
2015-03-30 14:25:32 +0200

    View and template for editing existing customers added

--- customers/rating/views.py
+++ customers/rating/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.core.urlresolvers import reverse
 from rating.models import CustomerRating
 
@@ -16,4 +16,12 @@ class CustomerCreate(CreateView):
     template_name = 'rating/edit_customer.html'
     fields = ['first_name', 'last_name', 'email', 'rating', 'contacted']
     success_url = '/'
+    
+class CustomerUpdate(UpdateView):
+    model = CustomerRating
+    fields = ['first_name', 'last_name', 'email', 'rating', 'contacted']
+    template_name = 'rating/update_customer.html'
+    success_url = '/'
+    
+
 
