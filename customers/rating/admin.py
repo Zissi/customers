69a04f8f3f9119baa5f8192a8f4d2a67bf79c760
Dominik <wahlholz@gmx.de>
2015-03-30 11:02:48 +0200

    CustomerRating model registered and __str__ method added

--- customers/rating/admin.py
+++ customers/rating/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from rating.models import CustomerRating
 
-# Register your models here.
+admin.site.register(CustomerRating)
--- customers/rating/models.py
+++ customers/rating/models.py
@@ -4,5 +4,9 @@ class CustomerRating(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    rating = models.IntegerField(blank=True)
+    rating = models.IntegerField(blank=True, null=True)
     contacted = models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
